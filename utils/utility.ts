de11669b8a11d2b85bc7f7444f4d45c4ebed237a
Papalexios <papalexios@gmail.com>
2025-08-08 18:40:46 +0300

    feat: update codebase with latest changes

---
+++ utils/utility.ts
@@ -0,0 +1,13 @@
+
+export const slugify = (text: string): string => {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-')       // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')   // Remove all non-word chars
+    .replace(/\-\-+/g, '-')     // Replace multiple - with single -
+    .replace(/^-+/, '')         // Trim - from start of text
+    .replace(/-+$/, '');        // Trim - from end of text
+};
+
+export const delay = (ms: number): Promise<void> => new Promise(res => setTimeout(res, ms));
